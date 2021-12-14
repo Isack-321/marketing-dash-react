37a13a299f60dc3a83210aa499d0917aded7611b
Isack <ituwei21@gmail.com>
2021-12-14 22:31:19 +0300

    changed contents of App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from "react";
+import "@blueprintjs/core/lib/css/blueprintjs.css";
+import {Navbar, Alignment, Divider} from "@blueprintjs/core";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="bp3-dark" style={{ position:"fixed", top: "0"}}>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading style={{marginLeft: "30px"}}>
+            Marketing Dashboard
+          </Navbar.Heading>
+
+        </Navbar.Group>
+      
+      </Navbar>
+
+      {}
+      <div style={{ marginLeft: "30px", marginTop: "80px", marginRight: "30px"}}>
+        <div style={{marginTop: "50px"}}>
+          {/*youtube*/}
+        </div>
+
+        <Divider style={{ marginTop: "50px"}}></Divider>
+        <div style={{ marginTop: "50px"}}>
+          { /*Twitter Component*/}
+        </div>
+
+        <Divider style={{ marginTop: "50px"}}></Divider>
+      </div>
+      {/* footer*/}
+      <div>
+        <h5 style={{
+          fontSize: "medium",
+          textAlign: "left",
+          margin:"30px 30px 20px 30px",
+        }}>
+          powered by ituwei.org
+        </h5>
+      </div>
     </div>
   );
 }
