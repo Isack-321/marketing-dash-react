fa2e3da43984a42a3cc2c206e9c107ff02c7f4c1
Isack <ituwei21@gmail.com>
2021-12-15 13:33:03 +0300

    added twira and yt func in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "@blueprintjs/core/lib/css/blueprintjs.css";
 import {Navbar, Alignment, Divider} from "@blueprintjs/core";
+import Twitter from "./components/twitter";
+import Youtube from "./components/youtube";
 
+import { DummyData } from "./DummyData";
 import './App.css';
 
 function App() {
+  const { metric = {} } = DummyData;
   return (
     <div className="App">
       <Navbar className="bp3-dark" style={{ position:"fixed", top: "0"}}>
@@ -21,11 +25,17 @@ function App() {
       <div style={{ marginLeft: "30px", marginTop: "80px", marginRight: "30px"}}>
         <div style={{marginTop: "50px"}}>
           {/*youtube*/}
+          <Youtube
+            youtubeMetrics={metric.youtubeMetrics}
+            youtubeMinutesDay={metric.youtubeMinutesDay}
+            youtubeMinutesCountry={metric.youtubeMinutesCountry}
+          ></Youtube>
         </div>
 
         <Divider style={{ marginTop: "50px"}}></Divider>
         <div style={{ marginTop: "50px"}}>
           { /*Twitter Component*/}
+          <Twitter twitter={metric.twitter.data}></Twitter>
         </div>
 
         <Divider style={{ marginTop: "50px"}}></Divider>
